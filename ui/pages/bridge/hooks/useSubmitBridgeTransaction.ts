502ad9bf0688d1b2d826972965c25b7eaad041a8
Jongsun Suh <jongsun.suh@icloud.com>
2024-12-12 12:18:39 -0500

    Update import paths

--- ui/pages/bridge/hooks/useSubmitBridgeTransaction.ts
+++ ui/pages/bridge/hooks/useSubmitBridgeTransaction.ts
@@ -3,7 +3,7 @@ import { zeroAddress } from 'ethereumjs-util';
 import { useHistory } from 'react-router-dom';
 import { TransactionMeta } from '@metamask/transaction-controller';
 import { createProjectLogger, Hex } from '@metamask/utils';
-import { QuoteMetadata, QuoteResponse } from '../types';
+import { QuoteMetadata, QuoteResponse } from '../../../../shared/types/bridge';
 import { DEFAULT_ROUTE } from '../../../helpers/constants/routes';
 import { setDefaultHomeActiveTabName } from '../../../store/actions';
 import { startPollingForBridgeTxStatus } from '../../../ducks/bridge-status/actions';
